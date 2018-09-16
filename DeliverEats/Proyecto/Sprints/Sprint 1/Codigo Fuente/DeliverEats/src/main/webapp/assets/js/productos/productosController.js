2310307179a36fe45dd2298ef9c3382a68d75e46
eluuugarcia <eluuugarcia@gmail.com>
2018-09-16 14:57:08 -0300

    productos harcodeados

--- DeliverEats/Proyecto/Sprints/Sprint 1/Codigo Fuente/DeliverEats/src/main/webapp/assets/js/productos/productosController.js
+++ DeliverEats/Proyecto/Sprints/Sprint 1/Codigo Fuente/DeliverEats/src/main/webapp/assets/js/productos/productosController.js
@@ -1,1 +1,14 @@
-﻿
+﻿angular.module("app").controller("productosController", function ($scope, productosService) {
+
+    
+
+    $scope.getProductos = function () {
+        productosService.getProductos(function (response) {
+            $scope.productos = response;
+        });
+    }
+
+    $scope.getProductos();
+    
+
+    });
