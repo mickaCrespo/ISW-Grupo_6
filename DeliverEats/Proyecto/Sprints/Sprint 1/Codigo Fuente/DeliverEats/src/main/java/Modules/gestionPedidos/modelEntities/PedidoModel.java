bacc17c755a615ac29794a17cc0b083da2fec8c6
mickaCrespo <mickaelacrespo@gmail.com>
2018-09-16 10:18:27 -0300

    Se agrega la primera clase

---
+++ DeliverEats/Proyecto/Sprints/Sprint 1/Codigo Fuente/DeliverEats/src/main/java/Modules/gestionPedidos/modelEntities/PedidoModel.java
@@ -0,0 +1,13 @@
+package Modules.gestionPedidos.modelEntities;
+
+import java.util.Date;
+
+public class PedidoModel {
+    private Float conCuantoPaga;
+    private String domicilio;
+    private Date entregaDeseada;
+    private Date fechaHora;
+    private String FormaDePago;
+    private Integer numeroPedido;
+
+}
