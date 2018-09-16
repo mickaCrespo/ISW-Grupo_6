9dd3b3066a2ab8663ec1056ea5b5a7deaa131790
mickaCrespo <mickaelacrespo@gmail.com>
2018-09-16 18:53:51 -0300

    Bug Fixes

---
+++ DeliverEats/Proyecto/Sprints/Sprint 1/Codigo Fuente/DeliverEats/src/main/java/Modules/RestHello.java
@@ -0,0 +1,16 @@
+package Modules;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+
+@Path("/test")
+public class RestHello {
+
+    @GET
+    @Path("/sayHello")
+    public String doGet() {
+        return "Hello World!";
+    }
+
+}
---
+++ DeliverEats/Proyecto/Sprints/Sprint 1/Codigo Fuente/DeliverEats/target/deliveryEat/assets/js/productos/productosController.js
@@ -0,0 +1,21 @@
+﻿angular.module("app").controller("productosController", function ($scope, productosService) {
+
+
+
+    $scope.getComercio = function () {
+        productosService.getComercio(function (response) {
+            $scope.comercio = response;
+        });
+    }
+
+    $scope.getProductos = function () {
+        productosService.getProductos(function (response) {
+            $scope.productos = response;
+        });
+    }
+
+    $scope.getComercio();
+    $scope.getProductos();
+    
+
+    });
