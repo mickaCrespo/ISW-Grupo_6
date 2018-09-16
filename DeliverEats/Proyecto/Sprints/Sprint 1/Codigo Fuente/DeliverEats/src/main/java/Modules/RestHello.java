51d9f6dbbbd94b364489513e38bf1419cefacf16
mickaCrespo <mickaelacrespo@gmail.com>
2018-09-16 19:37:08 -0300

    Bug Fixes

--- DeliverEats/Proyecto/Sprints/Sprint 1/Codigo Fuente/DeliverEats/src/main/java/Modules/RestHello.java
+++ DeliverEats/Proyecto/Sprints/Sprint 1/Codigo Fuente/DeliverEats/src/main/java/Modules/RestHello.java
@@ -3,9 +3,8 @@ package Modules;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
-
 @Path("/test")
-public class RestHello {
+public class RestHello{
 
     @GET
     @Path("/sayHello")
---
+++ DeliverEats/Proyecto/Sprints/Sprint 1/Codigo Fuente/DeliverEats/src/main/java/Modules/gestionComercios/services/ComerciosApp.java
@@ -0,0 +1,8 @@
+package Modules.gestionComercios.services;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/comercios")
+public class ComerciosApp extends Application{
+}
