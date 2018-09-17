7fe92c7d58a1f9d8141a5cd4a3d5f8043ca79eb0
juani15151 <15637690+juani15151@users.noreply.github.com>
2018-09-17 20:31:10 -0300

    La fecha del pedido se coloca automaticamente al crearlo. El detalle trae el precio actual del producto en el momento de creacion.

--- DeliverEats/Proyecto/Sprints/Sprint 1/Codigo Fuente Python/DeliveryEat/Main/models.py
+++ DeliverEats/Proyecto/Sprints/Sprint 1/Codigo Fuente Python/DeliveryEat/Main/models.py
@@ -14,7 +14,8 @@ class Articulo(models.Model):
 
 class Pedido(models.Model):
     numero = models.AutoField(primary_key=True)
-    fecha = models.DateTimeField()
+    # La fecha se define automaticamente cuando se crea el pedido.
+    fecha = models.DateTimeField(auto_now_add=True)
     direccion = models.CharField(max_length=256)
     entrega_deseada = models.CharField(max_length=256)
     forma_pago = models.CharField(max_length=256)
@@ -24,3 +25,8 @@ class DetallePedido(models.Model):
     articulo = models.ForeignKey('Articulo', on_delete=models.CASCADE)
     cantidad = models.PositiveIntegerField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def save(self, *args, **kwargs):
+        if self._state.adding:
+            self.precio = self.articulo.precio
+        super().save(*args, **kwargs)
