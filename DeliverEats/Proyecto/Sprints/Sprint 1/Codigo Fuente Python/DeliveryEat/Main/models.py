5226f6aaa1f0ea614e5f46128002ea6a8d760aa4
juani15151 <15637690+juani15151@users.noreply.github.com>
2018-09-17 15:16:01 -0300

    Cambio de politica on_delete en los modelos.

--- DeliverEats/Proyecto/Sprints/Sprint 1/Codigo Fuente Python/DeliveryEat/Main/models.py
+++ DeliverEats/Proyecto/Sprints/Sprint 1/Codigo Fuente Python/DeliveryEat/Main/models.py
@@ -6,7 +6,7 @@ class Comercio(models.Model):
     direccion = models.CharField(max_length=256)
 
 class Articulo(models.Model):
-    comercio = models.ForeignKey('Comercio')
+    comercio = models.ForeignKey('Comercio', on_delete=models.CASCADE)
     nombre = models.CharField(max_length=256)
     descripcion = models.CharField(max_length=256)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
@@ -20,7 +20,7 @@ class Pedido(models.Model):
     forma_pago = models.CharField(max_length=256)
 
 class DetallePedido(models.Model):
-    pedido = models.ForeignKey('Pedido')
-    articulo = models.ForeignKey('Articulo')
+    pedido = models.ForeignKey('Pedido', on_delete=models.CASCADE)
+    articulo = models.ForeignKey('Articulo', on_delete=models.CASCADE)
     cantidad = models.PositiveIntegerField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
