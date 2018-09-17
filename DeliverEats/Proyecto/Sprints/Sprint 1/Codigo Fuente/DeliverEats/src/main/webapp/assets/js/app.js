2d8dc32d9e8f48056c0d5877bb2c7431d4b1ad55
eluuugarcia <eluuugarcia@gmail.com>
2018-09-17 01:07:11 -0300

    Validaciones incluidas (falta testear)

--- DeliverEats/Proyecto/Sprints/Sprint 1/Codigo Fuente/DeliverEats/src/main/webapp/assets/js/app.js
+++ DeliverEats/Proyecto/Sprints/Sprint 1/Codigo Fuente/DeliverEats/src/main/webapp/assets/js/app.js
@@ -6,10 +6,6 @@ app.controller("deliverEatController", function ($scope, $http) {
     $scope.accion = "catalogo";
 
 
-    //Utilities...
-
-
-
 
     //Botones...
     $scope.continuarPedido = function () {
@@ -28,34 +24,116 @@ app.controller("deliverEatController", function ($scope, $http) {
 
     //Validaciones...
 
-    $scope.validarDatosForm = function (p, t) {
-        //Valida campos obligatorios completos
+    $scope.validarCamposObligatorios = function (p) {
         if ((p.direccion == undefined) || (p.paraCuando == undefined) || (p.formaPago == undefined)) {
             $scope.error = "Faltan completar datos...";
+            return false;
         }
-        //Validaciones para pago en efectivo
-        if (p.formaPago == 0) {
-            //Efectivo con el que se paga >= Total
-            if (p.efectivo < p.total) {
-                $scope.error = "El monto a pagar debe ser igual o menor al total...";
-            }
-        }
-        //Validaciones para pago con tarjeta
         else {
-            //Valida datos completos de la tarjeta
-            if ((t.nro == undefined) || (t.nombre == undefined) || (t.expiracionMes == undefined) || (t.expiracionAnio == undefined)) {
-                $scope.error = "Faltan completar datos de la tarjeta...";
+            $scope.error = "";
+            return true;
+        }
+    }
+
+    $scope.validarfechaEntrega = function (p) {
+        if (p.paraCuando == '1') {
+            if ((p.fecha == undefined) || (p.hora == undefined)) {
+                $scope.error = "Faltan completar datos...";
+                return false;
             }
             else {
-               /* var fecha = new Date;
-                fecha.setUTCFullYear(t.expiracionAnio, t.expiracionMes, 01);*/
-
+                //Valida que la fecha y hora de entrega sean posteriores a la actual
+                var today = new Date();
+                var fecha = new Date();
+                fecha.setHours(p.hora.getHours(), p.hora.getMinutes(), 0, 0);
+                if (p.fecha < today) {
+                    $scope.error = "Fecha de entrega incorrecta...";
+                    return false;
+                }
+                else {
+                    if (fecha < today) {
+                        $scope.error = "Hora de entrega incorrecta...";
+                        return false;
+                    }
+                    else {
+                        $scope.error = "";
+                        return true;
+                    }
+                }
             }
         }
-
-        
-
+        else {
+            return true;
+        }
     }
+
+    $scope.validarpagoEfectivo = function (p) {
+        if (p.efectivo === undefined) {
+            $scope.error = "Ingrese el monto con el que va a pagar...";
+            return false;
+        }
+        else {
+            
+            if (p.efectivo < p.total) {
+                $scope.error = "El monto a pagar debe ser igual o menor al total...";
+                return false;
+            }
+            else {
+                $scope.error = "";
+                return true;
+            }
+
+        }
+    }
+
+    $scope.validarpagoTarjeta = function (p) {
+        //Valida datos completos de la tarjeta
+        if ((p.nroTarjeta == null) || (p.nombreTarjeta === undefined) || (p.expiracionMes === undefined) || (p.expiracionAnio === undefined) || (p.codTarjeta === undefined)) {
+            alert(p.nroTarjeta);
+            $scope.error = "Faltan completar datos de la tarjeta...";
+            return false;
+        }
+        else {
+            //Valida que la fecha de expiración no haya pasado
+            var today = new Date();
+            var fecha = new Date();
+            fecha.setUTCFullYear(p.expiracionAnio, p.expiracionMes, null);
+            if (fecha < today) {
+                $scope.error = "Fecha de expiración incorrecta...";
+                return false;
+            }
+            else {
+                $scope.error = "";
+                return true;
+
+            }
+
+        }
+    }
+
+
+    $scope.validarDatosForm = function (p) {
+        //Si los campos obligatorios estan completos...
+        if ($scope.validarCamposObligatorios(p) == true) {
+            //Si la entrega es programada...
+            if ($scope.validarfechaEntrega(p) == true) {
+                //Validaciones para pago en efectivo
+                if (p.formaPago == 0) {
+                    if ($scope.validarpagoEfectivo(p) == true) {
+                        alert("pedido listo para ser realizado...");
+                    }
+
+                }
+                else {
+                    if ($scope.validarpagoTarjeta(p) == true) {
+                        alert("pedido listo para ser realizado...");
+                    }
+
+                }
+            }
+        }
+    }
+
 
 });
 
