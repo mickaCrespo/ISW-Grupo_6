796804502946c06439fe55d6171d411cadcd2e5d
juani15151 <15637690+juani15151@users.noreply.github.com>
2018-09-17 22:05:33 -0300

    Eliminados los ejemplos.

--- DeliverEats/Proyecto/Sprints/Sprint 1/Codigo Fuente Python/DeliveryEat/webapp/static/assets/js/app.js
+++ DeliverEats/Proyecto/Sprints/Sprint 1/Codigo Fuente Python/DeliveryEat/webapp/static/assets/js/app.js
@@ -207,51 +207,5 @@ app.controller("deliverEatController", function ($scope, $http) {
     }
 
 });
-    /* EJEMPLO DE POST
-    var comercio ={
-            "nombre" : "Jaime",
-            "descripcion" : "Hola",
-            "direccion" : "Ahi"
-        }
-    ;
-    $http.post('/api/comercios/', comercio, {
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-    }).then(function (response) {
-        alert(response);
-    }, function error(e) {
-        console.log(e);
-    });
-    */
-    /*//EJEMPLO DE POST PEDIDO
-    var pedido ={
-            "fecha" : "2018-01-01T00:00",
-            "direccion" : "Hola",
-            "entrega_deseada" : "Ahi",
-            "forma_pago" : "Asi",
-            "detalles" : [{
-                "articulo_id" : 1,
-                "cantidad" : 1,
-                "precio" : 10
-            },
-            {
-                "articulo_id" : 2,
-                "cantidad" : 1,
-                "precio" : 10
-            }
-            ]
-        }
-    ;
-    $http.post('/api/pedidos/', pedido, {
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-    }).then(function (response) {
-        alert(response);
-    }, function error(e) {
-        console.log(e);
-    });
-    //*/
 
 
